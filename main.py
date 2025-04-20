0c23222ebd9e9171f42a716f89a69d18e9e3c686
Henry4799 <henry.enholm@gmail.com>
2025-04-20 13:59:45 -0400

    Added player rotation

--- main.py
+++ main.py
@@ -10,23 +10,22 @@ print(f"Screen height: {SCREEN_HEIGHT}")
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 dt = 0
 fps = pygame.time.Clock()
+player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, PLAYER_RADIUS)
 
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    x = SCREEN_WIDTH/2
-    y = SCREEN_HEIGHT/2
+
+    player.update(dt)
 
     fill_color = (0,0,0)
     screen.fill(fill_color, rect=None)
-
-    player = Player(x, y, PLAYER_RADIUS)
     player.draw(screen)
     pygame.display.flip()
-    dt = fps.tick(60)/1000
 
+    dt = fps.tick(60)/1000
 
 if __name__ == "__main__":
     main()
