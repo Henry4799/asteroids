62077e6830921db1e4ff47a44e3363e958bd2a1b
Henry4799 <henry.enholm@gmail.com>
2025-04-20 13:31:00 -0400

    Added circleshape

--- main.py
+++ main.py
@@ -6,16 +6,20 @@ print(f"Screen width: {SCREEN_WIDTH}")
 print(f"Screen height: {SCREEN_HEIGHT}")
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+dt = 0
+fps = pygame.time.Clock()
 
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-            
+
     fill_color = (0,0,0)
     screen.fill(fill_color, rect=None)
     pygame.display.flip()
+    dt = fps.tick(60)/1000
+
 
 if __name__ == "__main__":
     main()
